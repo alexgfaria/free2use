2a61e07afeea505c85f216336e33394ce2815137
Joaquim Perez <jquim1@hotmail.com>
2018-04-30 21:11:33 +0100

    Restructure

---
+++ js/conta/login.js
@@ -0,0 +1,33 @@
+$("document").ready(function(e)
+{
+	$("#iniciar").click(function(e)
+	{
+		var user = $("#user").val();
+		var pass = $("#pass").val();
+
+		if(user == "aluno" && pass == "aluno")
+		{
+			$(".container").load("html/menu_principal/aluno.html");
+		}
+		else if(user == "prof" && pass == "prof")
+		{
+			$(".container").load("html/menu_principal/prof.html");
+		}
+		else if(user == "tecnico" && pass == "tecnico")
+		{
+			$(".container").load("html/menu_principal/tecnico.html");
+		}
+		else
+		{
+			alert("Credenciais erradas. Tente novamente.");
+		}
+	});
+	$("#criar").click(function(e)
+	{
+		$(".container").load("html/conta/criar.html");
+	});
+	$("#esquecer").click(function(e)
+	{
+		$(".container").load("html/conta/esquecer.html");
+	});
+});
