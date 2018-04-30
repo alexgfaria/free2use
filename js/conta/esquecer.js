f4bb114ff635cbe97809ddebbf349242d845b793
Joaquim Perez <jquim1@hotmail.com>
2018-04-30 21:07:57 +0100

    'esquecer' goes back to the main page

--- js/conta/esquecer.js
+++ js/conta/esquecer.js
@@ -2,6 +2,7 @@ $("document").ready(function(e)
 {
 	$("#submit-email").click(function(e)
 	{
-		alert("");
+		alert("Foi enviado um email com as instruções para repor a sua palavra-passe.");
+		$(".container").load("index.html");
 	});
 });
