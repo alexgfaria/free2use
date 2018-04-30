9c4171f218b562a328645d60c0a5999964dd47eb
Joaquim Perez <jquim1@hotmail.com>
2018-04-30 21:23:17 +0100

    Possible issue solved (2)

--- js/conta/esquecer.js
+++ js/conta/esquecer.js
@@ -3,6 +3,6 @@ $("document").ready(function(e)
 	$("#submit-email").click(function(e)
 	{
 		alert("Foi enviado um email com as instruções para repor a sua palavra-passe.");
-		$(".container").load("index.html");
+		$(".container").load("html/conta/login.html");
 	});
 });
