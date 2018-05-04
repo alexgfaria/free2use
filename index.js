7396c3349408c6a220b989207fe77b6526c2a7ea
Joaquim Perez <jquim1@hotmail.com>
2018-05-04 15:30:45 +0100

    logout button handling corrected

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-$("document").ready(function(e)
+function goToHome()
 {
 	// O botão do 'logout' é escondido
-	//$("#logout").hide();
+	$("#logout").hide();
 
 	// É mostrada a página de login
 	$(".container").load("html/conta/login.html");
+}
+
+$("document").ready(function(e)
+{
+	goToHome();
 
 	// Ao clicar no botão 'home'
 	$(".glyphicon-home").click(function(e)
 	{
-		// Volta para a página de login
-		$(".container").load("html/conta/login.html");
+		goToHome();
 	});
 });
