e30d47cf293cf92bd1934e654beab04bd95fe9b2
Joaquim Perez <jquim1@hotmail.com>
2018-05-05 14:55:15 +0100

    tecnico init

--- js/menu_principal/tecnico.js
+++ js/menu_principal/tecnico.js
@@ -1,4 +1,27 @@
 $("document").ready(function(e)
 {
+	// Fechar sala de estudo
+	$("#fechar").click(function(e)
+	{
+		alert("WIP-FECHAR");
+	});
+
+	// Abrir sala de estudo
+	$("#abrir").click(function(e)
+	{
+		alert("WIP-ABRIR");
+	});
+
+	// Adicionar sala de estudo
+	$("#adicionar").click(function(e)
+	{
+		alert("WIP-ADICIONAR");
+	});
+
+	// Remover sala de estudo
+	$("#remover").click(function(e)
+	{
+		alert("WIP-REMOVER");
+	});
 
 });
