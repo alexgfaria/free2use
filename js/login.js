f2ec7df75ee79aed1100925e15331f2bf65864f5
Joaquim Perez <jquim1@hotmail.com>
2018-05-09 17:55:56 +0100

    home button done

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ function loadMenuPrincipal(tipo)
 
 	// Ação do botão 'home'
 	// (volta para a respetiva página principal)
-	$(".glyphicon-home").click(function(e)
+	$(".navbar-brand").click(function(e)
 	{
 		goToHome(tipo);			
 	});
