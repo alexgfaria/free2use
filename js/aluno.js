380d612db6c757456dd41a21eb9973516c7d86d6
Joaquim Perez <jquim1@hotmail.com>
2018-05-11 17:47:14 +0100

    cancelamento das reservas agora é alertado

--- js/aluno.js
+++ js/aluno.js
@@ -1,4 +1,33 @@
 $("document").ready(function(e)
 {
+	// ...
+	// add listeners
+	// ...
+
+	alertCancelation();
 
 });
+
+function randomBool()
+{ return Math.random() >= 0.5; }
+
+function alertCancelation()
+{
+	var isReservaCancelada = randomBool();
+	if(isReservaCancelada)
+	{
+		var tecnicoCancelou = randomBool();
+		var message_delay = (parseInt(Math.random() * 8) + 2)*1000;
+		setTimeout(function(e)
+		{
+			if(tecnicoCancelou)
+			{
+				alert("Uma ou mais das suas reservas foram canceladas devido a sala(s) de estudo(s) fechada(s)/removida(s).");
+			}
+			else
+			{
+				alert("Uma ou mais das suas reservas foram canceladas devido a uma reserva sobreposta por um docente.");
+			} 
+		},message_delay);	
+	}
+}
