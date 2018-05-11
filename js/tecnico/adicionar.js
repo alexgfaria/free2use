72ab89c45642abcf5e3397fc358730630853ff19
Miguel Pires <ma2pires@gmail.com>
2018-05-11 17:57:53 +0100

    OpConcluida

--- js/tecnico/adicionar.js
+++ js/tecnico/adicionar.js
@@ -1,13 +1,41 @@
-/*$("document").ready(function(e)
+$("document").ready(function(e)
 {
-	$(":button").click(function(e)
+	$("#action").click(function(e)
 	{
-    var x = document.body;
-        while (x.firstChild) {
-            x.removeChild(x.firstChild);
-        }
+		//load("html/tecnico/"+"concluido"+".html");
+		load("html/tecnico/"+"opconcluida"+".html");
 	});
 
 });
 
+
+$("document").ready(function(e)
+{
+	$("#hometec").click(function(e)
+	{
+		//load("html/tecnico/"+"concluido"+".html");
+		load("html/"+"tecnico"+".html");
+	});
+
+});
+
+$("document").ready(function(e)
+{
+	$("#retornar").click(function(e)
+	{
+		//load("html/tecnico/"+"concluido"+".html");
+		parent.history.back();
+	});
+
+});
+
+/*
+	<div class="form-group row">
+	     <div class="col-lg-5"> </div>
+	     <div class="col-lg-2">
+        	<button type="button" class="btn-info btn-lg" id="retornar">
+          		<span class="glyphicon glyphicon-arrow-left"></span>Re 
+       		</button>
+   		 </div>
+    </div>
 */
